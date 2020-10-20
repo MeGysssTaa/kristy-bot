7b23556a607d256fcb8fb267af24391d9de962a3
MeowAntony <antonsmirnov2233@gmail.com>
2020-10-20 22:56:11 +0200

    добавил ещё одну картинку (больше не буду)

--- kristy-bot.py
+++ kristy-bot.py
@@ -558,6 +558,9 @@ for event in vklong.listen():
                 vk.messages.send(chat_id=event.chat_id, attachment="photo-199300529_457239031", random_id=int(vk_api.utils.get_random_id()))
             elif command == "пиздец":
                 vk.messages.send(chat_id=event.chat_id, attachment="photo-199300529_457239032", random_id=int(vk_api.utils.get_random_id()))
+            elif command == "похуй":
+                vk.messages.send(chat_id=event.chat_id, attachment="photo-199300529_457239033", random_id=int(vk_api.utils.get_random_id()))
+
         #проверка пингов без +
         if re.findall(r"(?:\s|^)\@([a-zA-Zа-яА-ЯёЁ\d]+)(?=\s|$)", event.object.message["text"]):
             pinglist = []
