2bca72c7bbf6d827abb76d4b0bb214bb4f483359
MeowAntony <antonsmirnov2233@gmail.com>
2020-11-05 23:30:10 +0200

    !добавил flex

--- kristy-bot.py
+++ kristy-bot.py
@@ -672,6 +672,9 @@ for event in vklong.listen():
                 vk.messages.send(chat_id=event.chat_id, attachment="photo-199300529_457239154", random_id=int(vk_api.utils.get_random_id()))
             elif command == "окей":
                 vk.messages.send(chat_id=event.chat_id, message="мы просто играем в жизнь", random_id=int(vk_api.utils.get_random_id()))
+            elif command == "флекс":
+                vk.messages.send(chat_id=event.chat_id, attachment="photo-199300529_457239168", random_id=int(vk_api.utils.get_random_id()))
+
 
         #проверка пингов без +
         if re.findall(r"(?:\s|^)\@([a-zA-Zа-яА-ЯёЁ\d]+)(?=\s|$)", event.object.message["text"]):
