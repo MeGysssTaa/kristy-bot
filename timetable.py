0793d6a923f071f7c31d27087b8b999c24258581
DarksideCode <darksidecode@ya.ru>
2020-11-05 22:45:32 +0200

    Попытка пофиксить сравнение tzNaive/tzAware времён

--- timetable.py
+++ timetable.py
@@ -94,6 +94,11 @@ def __is_cur_time_in_range(now, start_tstr, end_tstr):
     """
     start = now.combine(now.date(), datetime.strptime(start_tstr, CLASS_TIME_FMT).time())
     end = now.combine(now.date(), datetime.strptime(end_tstr, CLASS_TIME_FMT).time())
+    KALININGRAD_TZ.localize(start)
+    KALININGRAD_TZ.localize(end)
+
+    if now.tz_info is None:
+        KALININGRAD_TZ.localize(now)
 
     return start <= now <= end
 
