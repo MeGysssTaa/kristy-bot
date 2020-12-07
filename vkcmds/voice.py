171fcf9bce2eeb3aa287c535615478aa21b43e9c
MeowAntony <antonsmirnov2233@gmail.com>
2020-12-07 17:20:18 +0200

    добавил топ 5 по общей длительности

--- vkcmds/voice.py
+++ vkcmds/voice.py
@@ -36,4 +36,23 @@ class ChooseChat(VKCommand):
             if dict_ids[user['id']][1] % 60 > 0:
                 response += ' ' + str(dict_ids[user['id']][1] % 60) + ' с.'
             response += ') \n'
+        voices_new = sorted(voices, key=lambda voice: voice["voice_duration"], reverse=True)
+        dict_ids = {}
+        for number, voice in enumerate(voices_new):
+            if number == 5:
+                break
+            dict_ids.update({voice["user_id"]: [voice["voice_count"], voice["voice_duration"]]})
+        users_vk = self.kristy.vk.users.get(user_ids=list(dict_ids.keys()))
+        response += "Топ 5 по общей длительности: \n"
+        for number, user in enumerate(users_vk):
+            response += str(number + 1) + ". {0} {1} (⌛".format(user['first_name'], user['last_name'])
+            if dict_ids[user['id']][1] // 86400 > 0:
+                response += ' ' + str(dict_ids[user['id']][1] // 86400) + ' дн.'
+            if dict_ids[user['id']][1] % 86400 // 3600 > 0:
+                response += ' ' + str(dict_ids[user['id']][1] % 86400 // 3600) + ' ч.'
+            if dict_ids[user['id']][1] % 3600 // 60 > 0:
+                response += ' ' + str(dict_ids[user['id']][1] % 3600 // 60) + ' м.'
+            if dict_ids[user['id']][1] % 60 > 0:
+                response += ' ' + str(dict_ids[user['id']][1] % 60) + ' с.'
+            response += ') \n'
         self.kristy.send(peer, response)
