42c3b7c04e71724abb08f3e0f723c842a0a4ccc7
MeowAntony <antonsmirnov2233@gmail.com>
2020-12-11 23:58:58 +0200

    изменил ранги участников (теперь показывает, кому бот может писать)

--- vkcmds/rank_members_keyboard.py
+++ vkcmds/rank_members_keyboard.py
@@ -14,13 +14,13 @@ class ChooseChat(VKCommand):
     def execute(self, chat, peer, sender, args=None, attachments=None):
 
         users = self.kristy.db.get_users(chat)
-        users_vk = self.kristy.vk.users.get(user_ids=users)
-        users_sorted = [[user["first_name"] + ' ' + user["last_name"], self.kristy.db.get_user_rank_val(chat, user["id"])] for user in sorted(users_vk, key=lambda user: (user["first_name"], user["last_name"]))]
+        users_vk = self.kristy.vk.users.get(user_ids=users, fields=["can_write_private_message"])
+        users_sorted = [[user["first_name"] + ' ' + user["last_name"], self.kristy.db.get_user_rank_val(chat, user["id"]), user['can_write_private_message']] for user in sorted(users_vk, key=lambda user: (user["first_name"], user["last_name"]))]
 
         users_with_rank_sorted = sorted(users_sorted, key=lambda user: user[1], reverse=True)
         king = users_with_rank_sorted[0]
         users_with_rank_sorted = users_with_rank_sorted[1:]
-        response = 'KING: {0} \n'.format(king[0])
+        response = 'KING: {0} {1} \n'.format(king[0], '✅' if king[2] else '')
         rank_now = ""
         number = 1
         for user in users_with_rank_sorted:
@@ -28,7 +28,7 @@ class ChooseChat(VKCommand):
                 rank_now = ranks.Rank(user[1]).name
                 number = 1
                 response += "\n{0}: \n".format(rank_now)
-            response += '{0}. {1} \n'.format(str(number), user[0])
+            response += '{0}. {1} {2} \n'.format(str(number), user[0], '✅' if user[2] else '')
             number += 1
         if users_with_rank_sorted:
             self.kristy.send(peer, response, [], keyboards.information_keyboard(chat))
