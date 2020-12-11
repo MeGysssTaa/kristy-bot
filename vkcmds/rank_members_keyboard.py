25d265303016d923e13e2782876a78bb04df9555
MeowAntony <antonsmirnov2233@gmail.com>
2020-12-12 00:09:27 +0200

    изменил ранги участников (теперь показывает, кому бот может писать) 4

--- vkcmds/rank_members_keyboard.py
+++ vkcmds/rank_members_keyboard.py
@@ -20,7 +20,7 @@ class ChooseChat(VKCommand):
         users_with_rank_sorted = sorted(users_sorted, key=lambda user: user[1], reverse=True)
         king = users_with_rank_sorted[0]
         users_with_rank_sorted = users_with_rank_sorted[1:]
-        response = 'KING: {0} {1} \n'.format(king[0], '❌' if not king[2] else '')
+        response = 'KING: {0} {1} \n'.format(king[0], '♿' if not king[2] else '')
         rank_now = ""
         number = 1
         for user in users_with_rank_sorted:
@@ -28,7 +28,7 @@ class ChooseChat(VKCommand):
                 rank_now = ranks.Rank(user[1]).name
                 number = 1
                 response += "\n{0}: \n".format(rank_now)
-            response += '{0}. {1} {2} \n'.format(str(number), user[0], '❌' if not user[2] else '')
+            response += '{0}. {1} {2} \n'.format(str(number), user[0], '♿' if not user[2] else '')
             number += 1
         if users_with_rank_sorted:
             self.kristy.send(peer, response, [], keyboards.information_keyboard(chat))
