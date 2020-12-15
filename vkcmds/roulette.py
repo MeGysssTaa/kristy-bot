95d2658b3e2ea74a85e2b138a0f1085de88b7d75
MeowAntony <antonsmirnov2233@gmail.com>
2020-12-16 01:51:20 +0200

    ух, новая рулетка

--- vkcmds/roulette.py
+++ vkcmds/roulette.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 import ranks
 from vkcommands import VKCommand
 
@@ -14,12 +14,21 @@ class Roulette(VKCommand):
     def execute(self, chat, peer, sender, args=None, attachments=None):
 
         users = self.kristy.db.get_users(chat)
-        random_user = users[os.urandom(1)[0] % len(users)]
-        users = self.kristy.vk.users.get(user_ids=[sender, random_user], fields=["photo_id", "photo_max_orig"]).copy()
-        imposter, user = users if sender != random_user else users*2
-        response = "{0} {1} делает выстрел в:".format(imposter["first_name"], imposter["last_name"])
-        if not user["is_closed"] and "photo_id" in user:
-            self.kristy.send(peer, response, "photo" + user["photo_id"])
-        else:
-            list_attachments = self.kristy.get_list_attachments([{"type": "photo", "photo": {"sizes": [{"width": 400, "url": user["photo_max_orig"]}]}}], peer)
-            self.kristy.send(peer, response, list_attachments)
+        number = int(args[0]) if args and str(args[0]).isdigit() and 0 < int(args[0]) <= 10 else 10 if args and int(args[0]) > 10 else 1
+        random_users = [users[os.urandom(1)[0] % len(users)] for i in range(number)]
+        users = self.kristy.vk.users.get(user_ids=[sender] + random_users, fields=["photo_id", "photo_max_orig"]).copy()
+        imposter = users[0]
+        users_dict = {}
+        attachments = []
+        for user in random_users:
+            for user_vk in users:
+                if user == user_vk["id"]:
+                    if user_vk["id"] not in users_dict:
+                        if not user_vk["is_closed"] and "photo_id" in user_vk:
+                            users_dict.update({user_vk["id"]: "photo" + user_vk["photo_id"]})
+                        else:
+                            users_dict.update({user_vk["id"]: self.kristy.get_list_attachments([{"type": "photo", "photo": {"sizes": [{"width": 400, "url": user_vk["photo_max_orig"]}]}}], peer)[0]})
+                    attachments.append(users_dict[user_vk["id"]])
+
+        response = "{0} {1} делает {2} в:".format(imposter["first_name"], imposter["last_name"], "выстрелы" if len(random_users) > 1 else "выстрел")
+        self.kristy.send(peer, response, attachments)
