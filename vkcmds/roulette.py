37c023a3d6ff63308255ba512c14a8d33bb9f23e
MeowAntony <antonsmirnov2233@gmail.com>
2020-12-25 04:27:02 +0200

    теперь без выстрелов в челиков без аватарки

--- vkcmds/roulette.py
+++ vkcmds/roulette.py
@@ -14,20 +14,30 @@ class Roulette(VKCommand):
     def execute(self, chat, peer, sender, args=None, attachments=None):
         number = int(args[0]) if args and str(args[0]).isdigit() and 0 < int(args[0]) <= 10 else 10 if args and str(args[0]).isdigit() and int(args[0]) > 10 else 1
         users = self.kristy.db.get_users(chat)
-        random_users = [users[os.urandom(1)[0] % len(users)] for i in range(number)]
-        users = self.kristy.vk.users.get(user_ids=[sender] + random_users, fields=["photo_id", "photo_max_orig"]).copy()
-        imposter = users[0]
-        users_dict = {}
+        users = users[:1000] if len(users) > 1000 else users
+        users_vk = self.kristy.vk.users.get(user_ids=[sender] + users, fields=["photo_id", "photo_max_orig", "has_photo"]).copy()
+        imposter = users_vk[0]
         attachments = []
-        for user in random_users:
-            for user_vk in users:
-                if user == user_vk["id"]:
-                    if user_vk["id"] not in users_dict:
-                        if not user_vk["is_closed"] and "photo_id" in user_vk:
-                            users_dict.update({user_vk["id"]: "photo" + user_vk["photo_id"]})
-                        else:
-                            users_dict.update({user_vk["id"]: self.kristy.get_list_attachments([{"type": "photo", "photo": {"sizes": [{"width": 400, "url": user_vk["photo_max_orig"]}]}}], peer)[0]})
-                    attachments.append(users_dict[user_vk["id"]])
-
-        response = "{0} {1} делает {2} в:".format(imposter["first_name"], imposter["last_name"], "выстрелы" if len(random_users) > 1 else "выстрел")
+        users_dict = {}
+        while users_vk:
+            random_user = users_vk[os.urandom(1)[0] % len(users_vk)]
+            if not random_user["has_photo"]:
+                users_vk.remove(random_user)
+                continue
+            if random_user["id"] not in users_dict:
+                if not random_user["is_closed"] and "photo_id" in random_user:
+                    photo = "photo" + random_user["photo_id"]
+                else:
+                    photo = self.kristy.get_list_attachments([{"type": "photo", "photo": {"sizes": [{"width": 400, "url": random_user["photo_max_orig"]}]}}], peer)[0]
+                users_dict.update({random_user["id"]: photo})
+            else:
+                photo = users_dict[random_user["id"]]
+            attachments.append(photo)
+            if len(attachments) == number:
+                break
+        if not attachments:
+            self.kristy.send(peer, "У всех пользователей нет аватарок")
+            return
+        response = "{0} {1} делает {2} в:".format(imposter["first_name"], imposter["last_name"], "выстрелы" if len(attachments) > 1 else "выстрел")
         self.kristy.send(peer, response, attachments)
+
