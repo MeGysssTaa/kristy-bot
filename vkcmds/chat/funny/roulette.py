30a791020c164249779aa553f3d289c04bfac043
MeowAntony <antonsmirnov2233@gmail.com>
2021-01-02 03:11:58 +0200

    фиксим

--- vkcmds/chat/funny/roulette.py
+++ vkcmds/chat/funny/roulette.py
@@ -18,7 +18,7 @@ class Roulette(VKCommand):
         users = users[:1000] if len(users) > 1000 else users
         if chat not in self.kristy.killed:
             self.kristy.killed.update(({chat: {}}))
-        if sender in self.kristy.killed[chat] and self.kristy.killed[chat][sender] + TIME*60*60 < time.time():
+        if sender in self.kristy.killed[chat] and self.kristy.killed[chat][sender] + TIME*60*60 > time.time():
             return
         elif sender in self.kristy.killed[chat]:
             self.kristy.killed[chat].pop(sender)
