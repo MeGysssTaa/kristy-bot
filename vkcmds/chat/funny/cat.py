e196c0375a046db5754260ed5996627f8140d311
MeowAntony <antonsmirnov2233@gmail.com>
2021-06-25 15:46:00 +0200

    обновил !котик

--- vkcmds/chat/funny/cat.py
+++ vkcmds/chat/funny/cat.py
@@ -13,7 +13,7 @@ class Roulette(VKCommand):
                            desc='Показывает случайную картинку котика')
 
     def execute(self, chat, peer, sender, args=None, attachments=None, fwd_messages=None):
-        cat_image = requests.get('https://cataas.com/cat').content
+        cat_image = requests.get('https://cataas.com/cat?type=original').content
         with open('../tmp/cat{0}.png'.format(chat), 'wb') as handler:
             handler.write(cat_image)
         uploads = self.kristy.vk_upload.photo_messages(photos="../tmp/cat{0}.png".format(chat))[0]
