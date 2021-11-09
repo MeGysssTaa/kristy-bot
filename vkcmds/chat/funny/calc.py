2ea8603c9cd213a0e9e2e7c1234df3e2528d50fe
MeowAntony <antonsmirnov2233@gmail.com>
2021-11-09 06:09:46 +0200

    добавил новую команду !calc и удалил !текст

---
+++ vkcmds/chat/funny/calc.py
@@ -0,0 +1,28 @@
+import base64
+import os
+
+import requests
+
+import ranks
+from vkcommands import VKCommand
+
+
+class Calc(VKCommand):
+    def __init__(self, kristy):
+        VKCommand.__init__(self, kristy,
+                           label='calc',
+                           desc='Калькулятор (ура)',
+                           usage='!calc <выражение>',
+                           min_args=1,
+                           min_rank=ranks.Rank.PRO)
+
+    def execute(self, chat, peer, sender, args=None, attachments=None, fwd_messages=None):
+        msg = ' '.join(args)
+        data = {'in[]': msg, 'trig': 'deg', 'p': 0, 's': 0}
+        r = requests.post("https://web2.0calc.com/calc", data=data)
+        response = r.json()["results"][0]
+        with open(f'./{chat}.png', 'wb') as file:
+            file.write(base64.decodebytes(bytes(response["img64"], 'utf-8')))
+        uploads = self.kristy.vk_upload.photo_messages(photos=f'./{chat}.png')[0]
+        os.remove(f'./{chat}.png')
+        self.kristy.send(peer, "", f'photo{uploads["owner_id"]}_{uploads["id"]}')
