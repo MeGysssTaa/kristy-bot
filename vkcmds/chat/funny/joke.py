f33b274b20745ea59ef637ac09409a4a7642f271
MeowAntony <antonsmirnov2233@gmail.com>
2021-11-22 23:57:23 +0200

    обновил !анекдот

--- vkcmds/chat/funny/joke.py
+++ vkcmds/chat/funny/joke.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 from bs4 import BeautifulSoup
 from fuzzywuzzy import fuzz
@@ -8,6 +9,9 @@ import keyboards
 from PIL import Image, ImageDraw
 import requests
 from vkcommands import VKCommand
+
+MIN_PAGE = 0
+MAX_PAGE = 35
 
 
 class ChooseChat(VKCommand):
@@ -18,7 +22,10 @@ class ChooseChat(VKCommand):
 
     def execute(self, chat, peer, sender, args: str = None, attachments=None, fwd_messages=None):
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36'}
-        page_soup = BeautifulSoup(requests.get("https://www.anekdot.ru/random/anekdot/", headers=headers).text, 'html.parser')
-        items = page_soup.find_all('div', class_='text')
-        new_item = BeautifulSoup(str(items[0]).replace("<br/>", " "), features="html.parser")
+        random_number = random.SystemRandom().randint(MIN_PAGE, MAX_PAGE)
+        page_soup = BeautifulSoup(requests.get(f"https://anekdotov.net/ancomp/index-page-{random_number}.html", headers=headers).text, 'html.parser')
+        items = page_soup.find_all('div', class_='anekdot')
+        item = random.SystemRandom().choice(items)
+        new_item = BeautifulSoup(str(item).replace("<br/>", "\n"), features="html.parser")
+        print(new_item.get_text())
         self.kristy.send(peer, new_item.get_text())
