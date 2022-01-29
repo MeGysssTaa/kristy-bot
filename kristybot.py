00520136f362e500adebe1f572e36313e9d60ee7
German Vekhorev <darksidecode@ya.ru>
2022-01-29 15:43:34 +0200

    Update kristybot.py

--- kristybot.py
+++ kristybot.py
@@ -19,7 +19,7 @@ import timetable_parser
 import vkcommands
 import vklistener
 
-VERSION = '2.6.8'  # версия бота (semantics: https://semver.org/lang/ru/)
+VERSION = '2.6.9'  # версия бота (semantics: https://semver.org/lang/ru/)
 
 MAX_MSG_LEN = 4096
 # FIXME временное решение
