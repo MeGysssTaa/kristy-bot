a58214bda5b299d80bb6eedc429de1409539bf98
DarksideCode <darksidecode@ya.ru>
2022-01-29 10:48:07 +0200

    Тест

--- kristybot.py
+++ kristybot.py
@@ -19,7 +19,7 @@ import timetable_parser
 import vkcommands
 import vklistener
 
-VERSION = '2.6.5'  # версия бота (semantics: https://semver.org/lang/ru/)
+VERSION = '2.6.6'  # версия бота (semantics: https://semver.org/lang/ru/)
 
 MAX_MSG_LEN = 4096
 # FIXME временное решение
