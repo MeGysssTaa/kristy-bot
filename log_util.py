6a3b57eb631906a685eea7fe0b9ac1eb03916b30
DarksideCode <darksidecode@ya.ru>
2022-01-29 10:53:25 +0200

    Тест

--- log_util.py
+++ log_util.py
@@ -18,25 +18,25 @@ def init_logging(module: str):
 
     logging_initialized.add(module)
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs/')
-
-    with open('logging.cfg.yml', 'r', encoding='UTF-8') as fstream:
-        # noinspection PyBroadException
-        try:
-            logging.config.dictConfig(yaml.safe_load(fstream))
-            logger = logging.getLogger(module)
-
-            # Т.к. suffix нельзя установить через конфиг, приходится делать так...
-            for handler in logger.handlers:
-                if type(handler) == logging.handlers.TimedRotatingFileHandler:
-                    handler.suffix = '%Y.%m.%d.log'
-
-            return logger
-        except Exception:
-            print('\n\n\nНе удалось настроить журналирование для модуля \"%s\":' % module)
-            traceback.print_exc()
-            print('\n\n\nВыход через 30 секунд...')
-            time.sleep(30)
-            print('\n\n\nВыход')
-            exit(1)
+    # if not os.path.exists('logs'):
+    #     os.mkdir('logs/')
+    #
+    # with open('logging.cfg.yml', 'r', encoding='UTF-8') as fstream:
+    #     # noinspection PyBroadException
+    #     try:
+    #         logging.config.dictConfig(yaml.safe_load(fstream))
+    #         logger = logging.getLogger(module)
+    #
+    #         # Т.к. suffix нельзя установить через конфиг, приходится делать так...
+    #         for handler in logger.handlers:
+    #             if type(handler) == logging.handlers.TimedRotatingFileHandler:
+    #                 handler.suffix = '%Y.%m.%d.log'
+    #
+    #         return logger
+    #     except Exception:
+    #         print('\n\n\nНе удалось настроить журналирование для модуля \"%s\":' % module)
+    #         traceback.print_exc()
+    #         print('\n\n\nВыход через 30 секунд...')
+    #         time.sleep(30)
+    #         print('\n\n\nВыход')
+    #         exit(1)
