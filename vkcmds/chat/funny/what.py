a48639f5bd88ca3173295aefd1748ecee1a733d9
MeowAntony <antonsmirnov2233@gmail.com>
2022-03-09 14:14:33 +0200

    изменил команду !что

--- vkcmds/chat/funny/what.py
+++ vkcmds/chat/funny/what.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -18,7 +20,12 @@ class Ruslan(VKCommand):
         if not attachments or attachments[0]["type"] != 'photo':
             self.kristy.send(peer, "Нету фотографии")
 
-        key = "3fdabd98ed01dd617bb39bb8899a70a2"
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/605.1.15 '
+                          '(KHTML, like Gecko) Version/14.1.1 Safari/605.1.15'
+        }
+
+        key = os.environ['IMAGE_KEY']
         max_photo_url = ""
         max_width = 0
         for photo in attachments[0]['photo']['sizes']:
@@ -28,7 +35,7 @@ class Ruslan(VKCommand):
         r = requests.post('https://api.imgbb.com/1/upload', params={"key": key, "image": max_photo_url})
         answer_api = r.json()
         url = f'https://yandex.ru/images/search?source=collections&rpt=imageview&url={answer_api["data"]["url"]}'
-        soup = BeautifulSoup(requests.get(url).text, features='html.parser')
+        soup = BeautifulSoup(requests.get(url, headers=headers).text, features='html.parser')
         similar = soup.find('section', class_='CbirItem CbirTags')\
                       .find('div', class_='Tags Tags_type_expandable Tags_view_buttons')\
                       .find('div').find_all('a')
