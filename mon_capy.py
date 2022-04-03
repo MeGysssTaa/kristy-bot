207c7acfccdced8938782e065741927a2d4a74de
German Vekhorev <darksidecode@ya.ru>
2022-04-03 21:40:09 +0200

    Капибары..

--- mon_capy.py
+++ mon_capy.py
@@ -2,7 +2,7 @@ import datetime
 import threading
 import time
 import traceback
-from typing import Optional
+from typing import Optional, Tuple
 
 import schedule as schedule
 
@@ -26,63 +26,65 @@ class MondayCapybara:
     def _run(self):
         # noinspection PyBroadException
         try:
-            club_id = -206143282  # https://vk.com/chill_capybaras
-            posts = self.kristy.vk_user.wall.get(owner_id=club_id, count=10)
+            video: Optional[Tuple[str, str]] = self._recent_mon_capy_vid()
 
-            if "is_pinned" in posts["items"][0] and posts["items"][0]["is_pinned"] == 1:
-                posts["items"] = posts["items"][1:]
+            if video is None:
+                return
 
-            for post in posts["items"]:
-                if "text" not in post or "attachments" not in post:
+            print('Found the Monday capybara video!!')
+
+            for chat in self.kristy.db.all_chat_ids():
+                #todo remove
+                if chat != 1:
                     continue
-
-                text = post["text"].lower()
-                print('.. ' + text)
-
-                # if "рубрик" not in text \
-                #         and "ванн" not in text \
-                #         and "таз" not in text \
-                #         and "вод" not in text\
-                #         and "купа" not in text \
-                #         and "понедельн" not in text:
-                #     continue
-
-                attachments = post["attachments"]
-                video: Optional[str] = None
-
-                for attachment in attachments:
-                    if "type" in attachment and attachment["type"] == "video":
-                        video = 'video{0}_{1}_{2}'.format(
-                            attachment['video']['owner_id'],
-                            attachment['video']['id'],
-                            attachment['video']['access_key']
-                        )
-
-                        break
-
-                if video:
-                    print('FOUND!')
-                    print(video)
-                    break
-
+                # todo remove
+                self._send_maybe(chat, video[0], video[1])
         except Exception:
             traceback.print_exc()
-        #
-        # for chat in self.kristy.db.all_chat_ids():
-        #     now: Optional[datetime] = timetable.curtime(self.kristy.tt_data, chat)
-        #
-        #     if now is None or now.weekday() != 0:  # 0: Понедельник
-        #         continue
-        #
-        #     last_capy_date: Optional[str] = self.kristy.db.get_last_capy_date()
-        #     now_str = f'{now.day}.{now.month}.{now.year}'
-        #
-        #     if last_capy_date == now_str:
-        #         continue
-        #
-        #     # noinspection PyBroadException
-        #     try:
-        #         self._exec(chat, now_str)
-        #     except Exception:
-        #         traceback.print_exc()
 
+    def _recent_mon_capy_vid(self) -> Optional[Tuple[str, str]]:
+        club_id = -206143282  # https://vk.com/chill_capybaras
+        posts = self.kristy.vk_user.wall.get(owner_id=club_id, count=10)
+
+        for post in posts["items"]:
+            if "text" not in post or "attachments" not in post:
+                continue
+
+            text = post["text"].lower()
+
+            # if "рубрик" not in text \
+            #         and "ванн" not in text \
+            #         and "таз" not in text \
+            #         and "вод" not in text\
+            #         and "купа" not in text \
+            #         and "понедельн" not in text:
+            #     continue
+
+            attachments = post["attachments"]
+
+            for attachment in attachments:
+                if "type" in attachment and attachment["type"] == "video":
+                    return text, 'video{0}_{1}_{2}'.format(
+                        attachment['video']['owner_id'],
+                        attachment['video']['id'],
+                        attachment['video']['access_key']
+                    )
+
+        return None
+
+    def _send_maybe(self, chat: int, text: str, video: str):
+        now: Optional[datetime] = timetable.curtime(self.kristy.tt_data, chat)
+
+        if now is None or now.weekday() != 0:  # 0: Понедельник
+            return
+
+        last_capy_date: Optional[str] = self.kristy.db.get_last_capy_date(chat)
+        now_str = f'{now.day}.{now.month}.{now.year}'
+
+        print(f'CHAT {chat} | last capy date : {last_capy_date}, now str : {now_str} -> {last_capy_date == now_str}')
+
+        if last_capy_date == now_str:
+            return
+
+        self.kristy.db.set_last_capy_date(chat, now_str)
+        self.kristy.send(peer=1E9+chat, msg=text, attachment=[video])
