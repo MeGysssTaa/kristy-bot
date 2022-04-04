2647c5c840dae2f1af9d7519985642fbfefc78b2
MeowAntony <antonsmirnov2233@gmail.com>
2022-04-04 18:22:37 +0200

    добавил !тайлер

--- vkcmds/chat/funny/tyler.py
+++ vkcmds/chat/funny/tyler.py
@@ -3,23 +3,18 @@ import re
 
 from vkcommands import VKCommand
 
-IDS_CAPYBARAS = [-208931251,  # https://vk.com/capybary
-                 -162903355,  # https://vk.com/kapibaryanstvo
-                 -206744223,  # https://vk.com/capybaraclub
-                 -206143282,  # https://vk.com/chill_capybaras
-                 -201833277]  # https://vk.com/capyeveryday
+ID_TYLER = -203127230
 
 
-class Capybara(VKCommand):
+class Tyler(VKCommand):
     def __init__(self, kristy):
         VKCommand.__init__(self, kristy,
-                           label='капибара',
-                           desc='Показывает капибару (sponsored by German)')
+                           label='тайлер',
+                           desc='Показывает философию тайлера (sponsored by Matvey)')
 
     def execute(self, chat, peer, sender, args: str = None, attachments=None, fwd_messages=None):
         for i in range(5):
-            random_group_id = random.SystemRandom().choice(IDS_CAPYBARAS)
-            posts = self.kristy.vk_user.wall.get(owner_id=random_group_id, count=25)
+            posts = self.kristy.vk_user.wall.get(owner_id=ID_TYLER, count=31)
 
             if 'is_pinned' in posts["items"][0] and posts["items"][0]["is_pinned"] == 1:
                 posts["items"] = posts["items"][1:]
@@ -31,8 +26,8 @@ class Capybara(VKCommand):
             except Exception:
                 continue
 
-            self.kristy.send(peer, "", attachment=data)
+            self.kristy.send(peer, random_post["text"], attachment=data)
             return
-        
+
         self.kristy.send(peer, "Видимо сегодня без капибар =(")
 
