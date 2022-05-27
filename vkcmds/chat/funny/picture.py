4056f47bf403dbf17593e3e83d8396902e8646d0
MeowAntony <antonsmirnov2233@gmail.com>
2022-05-27 14:40:56 +0200

    короче делаем просто с перезарядкой (!картинка)

--- vkcmds/chat/funny/picture.py
+++ vkcmds/chat/funny/picture.py
@@ -1,7 +1,6 @@
 import json
 import random
-import urllib.request
-import undetected_chromedriver
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,8 +8,7 @@ import ranks
 from vkcommands import VKCommand
 
 
-
-class Roulette(VKCommand):
+class Picture(VKCommand):
     def __init__(self, kristy):
         VKCommand.__init__(self, kristy,
                            label='картинка',
@@ -23,25 +21,28 @@ class Roulette(VKCommand):
         if not args:
             self.kristy.send(peer, "Нету текста")
             return
-
         text = ' '.join(args)
-        driver = undetected_chromedriver.Chrome(headless=True)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/605.1.15 '
+                          '(KHTML, like Gecko) Version/14.1.1 Safari/605.1.15'
+        }
         request = requests.get('https://yandex.ru/images/search',
                                params={"text": text,
                                        "nomisspell": 1,
-                                       "noreask": 1})
-        driver.get(request.url)
+                                       "noreask": 1
+                                       },
+                               headers=headers)
+        soup = BeautifulSoup(request.text, 'html.parser')
+        try:
+            items_place = soup.find('div', {"class": "serp-list"})
 
+            items = items_place.find_all("div", {"class": "serp-item"})
 
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        items_place = soup.find('div', {"class": "serp-list"})
-        items = items_place.find_all("div", {"class": "serp-item"})
-
-        random_item = random.SystemRandom().choice(items)
-        data = json.loads(random_item.get("data-bem"))
-        image = data['serp-item']['img_href']
-        photo = self.kristy.get_list_attachments([{"type": "photo",
-                                                   "photo": {"sizes": [{"width": 400, "url": image}]}}], peer)
-        self.kristy.send(peer, "", photo)
-        driver.close()
-        driver.quit()
+            random_item = random.SystemRandom().choice(items)
+            data = json.loads(random_item.get("data-bem"))
+            image = data['serp-item']['img_href']
+            photo = self.kristy.get_list_attachments([{"type": "photo",
+                                                       "photo": {"sizes": [{"width": 400, "url": image}]}}], peer)
+            self.kristy.send(peer, "", photo)
+        except Exception:
+            self.kristy.send(peer, "Перезарядка")
